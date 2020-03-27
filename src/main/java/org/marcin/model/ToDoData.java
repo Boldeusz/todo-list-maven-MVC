7eb0ae5d1b8c65a90269b831562164edef0fe93f
Boldeusz <mnpower@poczta.onet.pl>
2020-03-27 15:15:14 +0100

    update

---
+++ src/main/java/org/marcin/model/ToDoData.java
@@ -0,0 +1,37 @@
+package org.marcin.model;
+
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ToDoData {
+
+    // == fields ==
+    private static int idValue = 1;
+    private final List<ToDoItem> items = new ArrayList<>();
+
+    // == constructors ==
+
+    public ToDoData() {
+    }
+
+    // == public methods ==
+    public List<ToDoItem> getItems(){
+        return Collections.unmodifiableList(items);
+    }
+    public void addItem(@NonNull ToDoItem toAdd){
+
+        // instead of @NonNull
+//        if (toAdd == null){
+//            throw new NullPointerException("toAdd is a required parameter.");
+//        }
+        toAdd.setId(idValue);
+        items.add(toAdd);
+        idValue++;
+    }
+    public void removeItem(int id){
+
+    }
+}
--- src/main/java/org/marcin/model/ToDoItem.java
+++ src/main/java/org/marcin/model/ToDoItem.java
@@ -1,14 +1,17 @@
 package org.marcin.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
 // == @Data lombok creates many methods insted of writing it!!! Superb
 @Data
+@EqualsAndHashCode(of = "id")
+// @EqualsAndHashCode(of = {"id", "title"})
 public class ToDoItem {
 
     // == fields ==
-    private String id;
+    private int id;
     private String title;
     private String details;
     private LocalDate deadline;
